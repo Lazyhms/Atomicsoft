bdbb4272f190abc53e384600c0f64642f426d983
HUMAOSHUAI <hu15150@126.com>
2025-05-29 23:58:54 +0800

    get value

--- src/NetCore/Extensions/EnumExtensions.cs
+++ src/NetCore/Extensions/EnumExtensions.cs
@@ -3,7 +3,11 @@
 public static class EnumExtensions
 {
     public static T? GetAttributeOfType<T>(this Enum enumValue) where T : Attribute
-        => enumValue.GetType().GetField(enumValue.ToString(), BindingFlags.Public | BindingFlags.Static)?.GetCustomAttribute<T>(false);
+    {
+        var type = enumValue.GetType();
+        var name = Enum.GetName(type, enumValue) ?? string.Empty;
+        return type.GetField(name, BindingFlags.Public | BindingFlags.Static)?.GetCustomAttribute<T>(false);
+    }
 
     public static string? GetDescription(this Enum enumValue)
         => enumValue.GetAttributeOfType<DescriptionAttribute>()?.Description;
